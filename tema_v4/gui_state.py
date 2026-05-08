16301e3053c710c499a27add6878070a2b749adc
Niculescu Alex-Cosmin <niculescualexcosmin@gmail.com>
2026-05-09 01:55:50 +0300

    feat: Add thread-safe GUI state management

---
+++ tema_v4/gui_state.py
@@ -0,0 +1,32 @@
+import config
+
+class GUIState:
+    def __init__(self):
+        # Simulation lifecycle
+        self.app_open = True        
+        self.sim_active = False     
+        self.main_speed = config.MAIN_BELT_SPEED
+        
+        # --- Advanced Logic Timers ---
+        self.laser_cooldown = config.LASER_COOLDOWN_SEC
+        self.push_duration = config.PUSH_DURATION_SEC
+        
+        # --- Robot Kinematics & Geometry ---
+        self.robot_drop_height = config.ROBOT_DROP_HEIGHT
+        self.robot_hover_clearance = config.ROBOT_HOVER_CLEARANCE
+        self.robot_reach = config.ROBOT_REACH
+        self.robot_base_deg_cube = config.ROBOT_BASE_DEG_CUBE
+        self.robot_base_deg_cylinder = config.ROBOT_BASE_DEG_CYLINDER
+        
+        # --- Palletizing Logic ---
+        self.pallet_spacing = 0.10  # 10cm default
+        self.pallet_grid_size = 3
+        
+        # Spawner & Telemetry
+        self.auto_spawn_enabled = False
+        self.spawn_request_queue = [] 
+        self.spawn_spacing = config.SPAWNER_TARGET_SPACING
+        self.last_prediction = "None"
+        self.last_confidence = 0.0
+        self.sim_time = 0.0
+        self.loop_latency = 0.0
